86f68c93082cbdf2c3102d707e663219d92859e8
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2023-10-14 23:10:31 +0300

    добавлен для входа в сайт

---
+++ login.php
@@ -0,0 +1,94 @@
+<?php 
+session_start();
+include("config.php");
+$error="";
+$msg="";
+if(isset($_REQUEST['login']))
+{
+	$email=$_REQUEST['email'];
+	$pass=$_REQUEST['pass'];
+	$pass= sha1($pass);
+	
+	if(!empty($email) && !empty($pass))
+	{
+		$sql = "SELECT * FROM user where uemail='$email' && upass='$pass'";
+		$result=mysqli_query($con, $sql);
+		$row=mysqli_fetch_array($result);
+		   if($row){
+			   
+				$_SESSION['uid']=$row['uid'];
+				$_SESSION['uemail']=$email;
+				header("location:index.php");
+				
+		   }
+		   else{
+			   $error = "<p class='alert alert-warning'>Email or Password doesnot match!</p> ";
+		   }
+	}else{
+		$error = '<div class="alert"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg><span>Заполните все поля пожалуйста</span></div>';
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Войти в аккаунт</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <html data-theme="winter"></html>
+</head>
+<body>
+<?php include("header.php");?>
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Войдите в ваш аккаунт<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      
+    </body>
+    </html></h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="#" method="POST">
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Пароль</label>
+          <div class="text-sm">
+            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Забыли пароль?</a>
+          </div>
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      <button class="btn btn-wide ml-20">Подтвердить</button>
+    </form>
+
+    <p class="mt-10 text-center text-sm text-gray-500">
+      У вас нет аккаунта?
+      <a href="registration.php" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Создать бесплатный аккаунт</a>
+    </p>
+  </div>
+</div>
+
+<?php include("footer.php");?>
+</body>
+</html>
