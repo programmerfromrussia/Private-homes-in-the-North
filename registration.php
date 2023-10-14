0b9248f1e49d7aa581fb3500a35e42a259756b86
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2023-10-14 23:09:40 +0300

    Добавлена базовая регистрация

---
+++ registration.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Покупка недвижимости на Севере России</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <html data-theme="winter"></html>
+</head>
+<body>
+<?php include("header.php");?>
+
+<?php include("footer.php");?>
+</body>
+</html>
