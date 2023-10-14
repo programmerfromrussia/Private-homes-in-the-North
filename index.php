a7f27a5eb07690d5e6af9874cbdbf1760da53270
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2023-10-14 23:02:54 +0300

    main file changed

--- index.php
+++ index.php
@@ -49,7 +49,29 @@
         </div>
     </div>
 
-
+    <div class="carousel w-full mt-20">
+  <div id="slide1" class="carousel-item relative w-full h-96 object-cover">
+    <img src="/images/home.jpg" class="w-full h-96 object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a> 
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src="images/prhome.jpg" class="w-full h-96 object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src="images/white.jpg" class="w-full h-96 object-cover" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+</div>
 <?php include("footer.php");?>
 </body>
 </html>
