3aa52882f3e2dc94cffedbaaf765ce449a56abd8
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2023-10-15 22:09:51 +0300

    minor changes

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if(isset($_REQUEST['login']))
 				
 		   }
 		   else{
-			   $error = "<p class='alert alert-warning'>Email or Password doesnot match!</p> ";
+			   $error = "<p class='alert alert-warning'>Электронная почта или пароль не совпадают!</p> ";
 		   }
 	}else{
 		$error = '<div class="alert"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg><span>Заполните все поля пожалуйста</span></div>';
